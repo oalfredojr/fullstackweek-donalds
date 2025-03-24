291cf9fa28061fec4dc13e255d98f01e279b8841
oalfredojr <contatoalfredo@live.com>
2025-03-24 20:16:20 -0300

    chore: sort nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
